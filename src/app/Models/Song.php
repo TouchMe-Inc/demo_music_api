504ea2fa43a4faec1c37887c8b952189b460b3e0
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-11-20 20:01:06 +0300

    Add Relations

--- src/app/Models/Song.php
+++ src/app/Models/Song.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Song extends Model
 {
     use HasFactory;
-    //
+
+    public function albums(): BelongsToMany
+    {
+        return $this->belongsToMany(Album::class)->withPivot('track_number');
+    }
 }
