459c85d1f38749f40d6b480323d08b442de451f6
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-11-20 20:01:01 +0300

    Add Relations

--- src/app/Models/Album.php
+++ src/app/Models/Album.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Album extends Model
 {
     use HasFactory;
-    //
+
+    public function artist(): BelongsTo
+    {
+        return $this->belongsTo(Artist::class);
+    }
+
+    public function songs(): BelongsToMany
+    {
+        return $this->belongsToMany(Song::class)->withPivot('track_number');
+    }
 }
