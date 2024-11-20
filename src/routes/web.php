581f8bf99669a19e7f0a9e515efd0ae5213b6feb
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-11-20 17:17:24 +0300

    Add routes for ./songs

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::apiResource('artists', ArtistController::class);
+Route::apiResource('songs', SongController::class);
