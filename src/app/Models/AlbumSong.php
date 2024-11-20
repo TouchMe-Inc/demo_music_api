27ef95ef31c05087f53eeb88dde83e85517fdca2
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-11-20 19:40:35 +0300

    Update AlbumSong as Pivot

--- src/app/Models/AlbumSong.php
+++ src/app/Models/AlbumSong.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class AlbumSong extends Model
+class AlbumSong extends Pivot
 {
-    //
+    protected $table = 'album_songs';
+
+    protected $fillable = ['album_id', 'song_id', 'track_number'];
 }
