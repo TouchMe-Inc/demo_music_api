4f13204bce86a05d5392cba87a72bb4f3634f063
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-11-20 16:45:47 +0300

    Init ArtistController

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ArtistController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('artists', ArtistController::class);
