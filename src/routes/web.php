1acf6205dff1b90b4c1c8ee3fd7bfaf2ea0af65c
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-11-20 19:41:44 +0300

    Add albumsongs routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\AlbumSongController;
 use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
@@ -12,3 +13,7 @@ Route::get('/', function () {
 Route::apiResource('artists', ArtistController::class);
 Route::apiResource('songs', SongController::class);
 Route::apiResource('albums', AlbumController::class);
+
+Route::get('albums/{album}/songs', [AlbumSongController::class, 'index']);
+Route::post('albums/{album}/songs', [AlbumSongController::class, 'store']);
+Route::delete('albums/{album}/songs/{song}', [AlbumSongController::class, 'destroy']);
