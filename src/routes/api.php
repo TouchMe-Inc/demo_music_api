c513a9554fad110ab349cac446ab2dc07d9c3cc9
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-11-21 12:58:16 +0300

    rename routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::apiResource('artists', ArtistController::class);
 Route::apiResource('songs', SongController::class);
 Route::apiResource('albums', AlbumController::class);
